49dfcbd15b9ca3abfba68e12129ca1f3cc603ab7
jookebawx <samuelsanjaya201@gmail.com>
2024-08-27 13:09:30 +0900

    test

---
+++ test.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract NFTregistry{
+    struct NFTInfo{
+        string chainid;
+        address contractaddress;
+        int tokenid;
+    }
+
+    mapping(string => NFTInfo) public nfts;
+    event NFTRegistered(string indexed filehash, string chainid, address indexed contractAddress, int tokenid);
+    
+    function registerNFT(string memory filehash,  string memory chain, address contractAddress, int tokenid) public {
+        require(contractAddress != address(0), "Invalid contract address");
+        require(nfts[filehash].contractaddress == address(0), "ID already used");
+        nfts[filehash] = NFTInfo(chain,contractAddress,tokenid);
+        emit NFTRegistered(filehash, chain ,contractAddress, tokenid);
+    }
+
+    function getNFTInfo(string memory filehash) public view returns (string memory, address, int) {
+        NFTInfo memory info = nfts[filehash];
+        require(info.contractaddress != address(0), "Contract not found");
+        return (info.chainid, info.contractaddress, info.tokenid);
+    } 
+
+    function isContractRegistered(string memory filehash) public view returns (bool) {
+        return nfts[filehash].contractaddress != address(0);
+    }       
+}
+
