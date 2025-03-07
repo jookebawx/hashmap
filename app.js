247d2daf1e05eb700cc7b7480ae5e37cb22be27f
jookebawx <110410554+jookebawx@users.noreply.github.com>
2025-03-08 00:59:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -29,4 +29,4 @@ window.connectMetaMask = connectMetaMask;
 window.verify = verify;
 window.registernft = registernft;
 window.fetchOwnerWithNetworkCheck = fetchOwnerWithNetworkCheck; // Expose this function
-window.displayChainResult = displayChainResult
+window.displayChainResult = displayChainResult;
