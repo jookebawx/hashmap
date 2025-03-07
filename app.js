8acab27c1916fd8b6ba2daa79108826894374018
jookebawx <110410554+jookebawx@users.noreply.github.com>
2025-03-08 00:58:08 +0900

    a

--- app.js
+++ app.js
@@ -29,3 +29,4 @@ window.connectMetaMask = connectMetaMask;
 window.verify = verify;
 window.registernft = registernft;
 window.fetchOwnerWithNetworkCheck = fetchOwnerWithNetworkCheck; // Expose this function
+window.displayChainResult = displayChainResult
